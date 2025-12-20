0a4d5c7e3128e02e93f1a6ea66cae4775779d942
leedoughty <32392044+leedoughty@users.noreply.github.com>
2025-12-20 10:59:12 +0000

    Add jukugo text component to intro

--- src/app/components/Intro.tsx
+++ src/app/components/Intro.tsx
@@ -1,11 +1,15 @@
 import styles from "../../styles/intro.module.css";
+import JukugoText from "./jukugoText";
 
 export default function Intro() {
   return (
-    <div className={styles.intro}>
-      Jukugo helps you learn Japanese compound words (熟語) through fun quizzes
-      and a handy kanji search. Explore, play, and use this tool however you
-      like, and enjoy a smoother Japanese learning journey!
+    <div className={styles.introWrapper}>
+      <JukugoText />
+      <div className={styles.intro}>
+        Jukugo helps you learn Japanese compound words (熟語) through fun
+        quizzes and a handy kanji search. Explore, play, and use this tool
+        however you like, and enjoy a smoother Japanese learning journey!
+      </div>
     </div>
   );
 }
---
+++ src/app/components/jukugoText.tsx
@@ -0,0 +1,5 @@
+import styles from "../../styles/jukugoText.module.css";
+
+export default function JukugoText() {
+  return <div className={styles.jukugoText}>熟語</div>;
+}
