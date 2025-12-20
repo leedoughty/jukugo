b8e2fe2ef776d829dd914678ae452e1be8561ea3
leedoughty <32392044+leedoughty@users.noreply.github.com>
2025-12-20 10:19:05 +0000

    Add intro component to homepage

---
+++ src/app/components/Intro.tsx
@@ -0,0 +1,11 @@
+import styles from "../../styles/intro.module.css";
+
+export default function Intro() {
+  return (
+    <div className={styles.intro}>
+      Jukugo helps you learn Japanese compound words (熟語) through fun quizzes
+      and a handy kanji search. Explore, play, and use this tool however you
+      like, and enjoy a smoother Japanese learning journey!
+    </div>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,12 @@
 
 import styles from "../styles/page.module.css";
 import QuizMenu from "./components/quizMenu";
+import Intro from "./components/Intro";
 
 export default function Home() {
   return (
     <div className={styles.page}>
+      <Intro />
       <div className={styles.flexRow}>
         <QuizMenu />
       </div>
